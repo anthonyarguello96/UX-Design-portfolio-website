59b59abca4034ba92377a6b6860289a4d56e8b8f
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-04-12 16:12:01 -0400

    Add HTML, CSS, JS
    This project was originally work on a different repository
    but my webpack configuration, was not working properly so
    in order to fix it I decided to start a new config in a new
    repository since I was not able to work out the configuration
    in the past repository. You can find the original repository
    in my GitHub account named as "UX-portfolio"

---
+++ dist/index.824413843b1a3f40ad4f.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module parse failed: Identifier 'lib' has already been declared (7:12)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n| lib.ItsWorking();\n| \n> import * as lib from './functions.js';\n| import './main.css';\n| import 'boxicons'");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,24 @@
 export const ItsWorking = () =>{
   alert("Hello world!");
 }
+
+//Toggles nav bar in and out of the viewport
+export const displayMenu =(toggleId, navId)=>{
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  if(toggle && nav){
+    console.log("I'm working");
+
+    toggle.addEventListener('click', ()=>{
+      nav.classList.toggle('show');
+    })
+  }
+}
+
+
+//Hides navigation menu after an element is clicked
+export const linkAction = () =>{
+const navMenu = document.getElementById('nav-menu');
+navMenu.classList.remove('show');
+}
